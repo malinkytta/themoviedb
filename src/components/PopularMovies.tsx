f0ddf87fe708412775180bcbce89d872f91e320a
Malin Kyttä <malinkytta@gmail.com>
2023-08-26 10:02:27 +0200

    Cleaned code, now map instead of [0], [1] ...

--- src/components/PopularMovies.tsx
+++ src/components/PopularMovies.tsx
@@ -15,7 +15,7 @@ const PopularMovies: React.FC<IProps> = ({ result }) => {
     const BASE_URL_IMAGE = 'https://image.tmdb.org/t/p/original'
 
     return (
-        <div>
+        <>
             {result && (
                 <>
                     <div className="d-flex justify-content-between pt-5">
@@ -29,55 +29,26 @@ const PopularMovies: React.FC<IProps> = ({ result }) => {
                     </div>
 
                     <Row xs={1} md={2} lg={3} className="g-4">
-                        <>
+                        {result.results.slice(0, 3).map(data => (
                             <Col className="d-flex align-items-stretch">
-
-                                <Card as={Link} to="/">
-                                    <Card.Img variant="top" src={BASE_URL_IMAGE + result.results[0].backdrop_path} />
+                                <Card as={Link} to={`movies/${data.id}`}>
+                                    <Card.Img variant="top" src={BASE_URL_IMAGE + data.backdrop_path} />
                                     <Card.Body>
                                         <Card.Title>
-                                            {result.results[0].title}
+                                            {data.title}
                                         </Card.Title>
                                         <Card.Text>
-                                            ⭐️ {result.results[0].vote_average}
+                                            ⭐️ {data.vote_average}
                                         </Card.Text>
                                     </Card.Body>
                                 </Card>
                             </Col>
-                            <Col className="d-flex align-items-stretch">
-
-                                <Card as={Link} to="/">
-                                    <Card.Img variant="top" src={BASE_URL_IMAGE + result.results[1].backdrop_path} />
-                                    <Card.Body>
-                                        <Card.Title>
-                                            {result.results[1].title}
-                                        </Card.Title>
-                                        <Card.Text>
-                                            ⭐️ {result.results[1].vote_average}
-                                        </Card.Text>
-                                    </Card.Body>
-                                </Card>
-                            </Col>
-
-                            <Col className="d-flex align-items-stretch">
-
-                                <Card as={Link} to="/">
-                                    <Card.Img variant="top" src={BASE_URL_IMAGE + result.results[2].backdrop_path} />
-                                    <Card.Body>
-                                        <Card.Title>
-                                            {result.results[2].title}
-                                        </Card.Title>
-                                        <Card.Text>
-                                            ⭐️ {result.results[2].vote_average}
-                                        </Card.Text>
-                                    </Card.Body>
-                                </Card>
-                            </Col>
-                        </>
+                        ))}
                     </Row>
+
                 </>
             )}
-        </div>
+        </>
     )
 }
 
