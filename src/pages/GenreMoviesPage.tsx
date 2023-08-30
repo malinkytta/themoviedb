8e99b7cad403e3df4911aacad6113bd5ee690e2d
Malin Kyttä <malinkytta@gmail.com>
2023-08-30 22:33:06 +0200

    LocalStorage for remembering genreTitle

--- src/pages/GenreMoviesPage.tsx
+++ src/pages/GenreMoviesPage.tsx
@@ -5,35 +5,37 @@ import Pagination from "../components/Pagination"
 import { useNavigate, useSearchParams } from "react-router-dom"
 import Dropdown from 'react-bootstrap/Dropdown'
 import DropdownButton from 'react-bootstrap/DropdownButton'
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Col from "react-bootstrap/Col"
 import Row from "react-bootstrap/Row"
 import Search from "../components/Search"
 import ErrorComponent from "../components/ErrorComponent"
+import useGenres from "../hooks/useGenres"
+import useSingleGenre from "../hooks/useSingleGenre"
 
 
 const GenreMoviesPage = () => {
     const [searchParams, setSearchParams] = useSearchParams()
-    const [title, setTitle] = useState('')
+    const [title, setTitle] = useState(localStorage.getItem("genreTitle") || "")
     const [searchInput, setSearchInput] = useState('')
     const navigate = useNavigate()
+
+
+    useEffect(() => {
+        localStorage.setItem("genreTitle", title)
+    }, [title])
+
+    useEffect(() => {
+        return () => {
+            localStorage.removeItem("genreTitle")
+        }
+    }, [])
 
     const genreId = searchParams.get('genre') ?? ''
     const page = Number(searchParams.get('page') ?? 1)
 
-    const newSearch = (page: string, genreId: string) => {
-        const newSearchParams = new URLSearchParams(searchParams.toString())
-        newSearchParams.set('page', String(page))
-        newSearchParams.set('genre', genreId)
-        setSearchParams(newSearchParams)
-    }
-
-    const genreTitles = useQuery(['genre',
-        { page }], () => getAllGenres(page))
-
-    const singleGenre = useQuery(['specific-genre',
-        { page, genre: genreId }],
-        () => getGenre(genreId, page))
+    const genreTitles = useGenres()
+    const singleGenre = useSingleGenre(genreId, page)
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault()
@@ -50,9 +52,10 @@ const GenreMoviesPage = () => {
             <ErrorComponent />
         )
     }
+    console.log(title)
 
     return (
-        <div>
+        <>
 
             <Search
                 handleSubmit={handleSubmit}
@@ -60,48 +63,47 @@ const GenreMoviesPage = () => {
                 setSearchInput={setSearchInput}
             />
 
-            {genreTitles.data && (
-
-                <DropdownButton variant="outline-secondary" data-bs-theme="dark" title="Filter genre">
-                    {genreTitles.data.map(data => (
-                        <Dropdown.Item
-                            key={data.id}
-                            onClick={() => {
-                                newSearch(String(1), String(data.id))
-                                setTitle(data.name)
-                            }}
-                        >
-                            {data.name}
-                        </Dropdown.Item>
-                    ))
-                    }
-                </DropdownButton>
-            )}
+            {
+                genreTitles.data && (
+                    <DropdownButton variant="outline-secondary" data-bs-theme="dark" title="Filter genre">
+                        {genreTitles.data.genres.map(data => (
+                            <Dropdown.Item
+                                key={data.id}
+                                onClick={() => {
+                                    setSearchParams({ page: String(page), genre: String(data.id) })
+                                    setTitle(data.name)
+                                }}
+                            >
+                                {data.name}
+                            </Dropdown.Item>
+                        ))
+                        }
+                    </DropdownButton>
+                )
+            }
 
             <Col xs={12} md={6}>
-                {title && (
-                    <h4 className="my-3">{title}</h4>
-                )}
+                <h4 className="my-3">{title}</h4>
             </Col>
 
-            {singleGenre.data && (
-                <>
-                    <Row>
-                        <Movies result={singleGenre.data} url={'movies/'} />
-
-                        <Pagination
-                            page={page}
-                            totalPages={singleGenre.data.total_pages}
-                            hasPreviousPage={page > 1}
-                            hasNextPage={page < (singleGenre.data.total_pages > 500 ? 500 : singleGenre.data.total_pages)}
-                            onPreviousPage={() => { newSearch(String(page - 1), genreId) }}
-                            onNextPage={() => { newSearch(String(page + 1), String(genreId)) }}
+            {
+                singleGenre.data && (
+                    <>
+                        {/* <Row> */}
+                        <Movies
+                            result={singleGenre.data}
+                            url={'movies/'}
+                            currentPage={page}
+                            setSearchParams={setSearchParams}
+                            data={singleGenre}
+                            time={''}
                         />
-                    </Row>
-                </>
-            )}
+                        {/* </Row> */}
+                    </>
+                )
+            }
 
-        </div >
+        </>
     )
 }
 
