b473da14c22363be19299ed4f15b43fb2bf1ca7e
Malin Kyttä <malinkytta@gmail.com>
2023-08-31 23:06:12 +0200

    Changed layout on homepage for popular movies

--- src/components/PopularMovies.tsx
+++ src/components/PopularMovies.tsx
@@ -1,10 +1,10 @@
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button'
 import { Link } from 'react-router-dom'
 import { MovieResponse } from '../types/movieAPI.types'
 import React from 'react'
+import Button from 'react-bootstrap/Button'
 
 interface IProps {
     result: MovieResponse
@@ -18,50 +18,33 @@ const PopularMovies: React.FC<IProps> = ({ result, toggleText, toggle }) => {
 
     return (
         <>
-
             {result && (
                 <>
-
-                    <div className="d-flex justify-content-start align-items-center px-3 py-4">
-                        <h2>Popular movies</h2>
-                        <div className="d-flex px-4">
-                            <p>Sort by</p>
-                            <Button
-                                data-bs-theme='dark'
-                                className=""
-                                variant="transparent"
-                                onClick={toggleText}
-                            >
-                                {toggle ? 'Day' : 'Week'}
-                            </Button>
-
-                        </div>
-                    </div>
-                    <div className='d-flex justify-content-end'>
+                    <h2 className="py-4 popular-movies-title">Popular movies</h2>
+                    <div className="d-flex px-4">
+                        <p>Sort by</p>
                         <Button
-                            className='px-end'
-                            variant="dark"
-                            href={`popular-movies?time-window=${toggle ? 'day' : 'week'}`}
+                            data-bs-theme='dark'
+                            className=""
+                            variant="transparent"
+                            onClick={toggleText}
                         >
-                            All movies &gt;
+                            {toggle ? 'Day' : 'Week'}
                         </Button>
                     </div>
-
-                    <Row xs={1} md={2} lg={3} className="g-4 mb-5">
-                        {result.results.slice(0, 3).map(data => (
-                            <Col key={data.id} className="d-flex align-items-stretch">
-                                <Card as={Link} to={`movies/${data.id}`}>
-                                    <Card.Img variant="top" src={data.backdrop_path
-                                        ? BASE_URL_IMAGE + data.backdrop_path
-                                        : 'https://placehold.co/500x300'
-                                    } />
-                                    <Card.Body>
-                                        <Card.Title>
-                                            {data.title}
-                                        </Card.Title>
-                                        <Card.Text>
-                                            ⭐️ {data.vote_average}
-                                        </Card.Text>
+                    <Row className="py-5 pb-3 mb-4 mt-4 g-3 slider flex-nowrap ms-3 popular-movies">
+                        {result.results.slice(0, 10).map(data => (
+                            <Col key={data.id} className=" d-flex align-items-stretch">
+                                <Card as={Link} to={`/popular/${data.id}`} className="movies-card">
+                                    <div className='poster'>
+                                        <Card.Img alt="poster" src={data.poster_path
+                                            ? BASE_URL_IMAGE + data.poster_path
+                                            : 'https://placehold.co/500x300'
+                                        } />
+                                    </div>
+                                    <Card.Body className='details'>
+                                        <Card.Title>{data.title}</Card.Title>
+                                        <Card.Text>   ⭐️ {data.vote_average}  </Card.Text>
                                     </Card.Body>
                                 </Card>
                             </Col>
