4f2122f3734083d875f5dcc8c1de4c8b86cb680e
Malin Kyttä <malinkytta@gmail.com>
2023-10-25 19:25:19 +0200

    Changed color on loadingspinner and placed in the center of the page

--- src/components/GlobalLoadingSpinner.tsx
+++ src/components/GlobalLoadingSpinner.tsx
@@ -1,14 +1,14 @@
-import { useIsFetching } from "@tanstack/react-query"
-import { SyncLoader } from "react-spinners"
+import { useIsFetching } from "@tanstack/react-query";
+import { SyncLoader } from "react-spinners";
 
 const GlobalLoadingSpinner = () => {
-    const isFetching = useIsFetching()
+  const isFetching = useIsFetching();
 
-    return isFetching ? (
-        <div id="global-loading-spinner-wrapper">
-            <SyncLoader speedMultiplier={1.5} />
-        </div>
-    ) : null
-}
+  return isFetching ? (
+    <div id="global-loading-spinner-wrapper">
+      <SyncLoader color="#cc5e48" speedMultiplier={1.5} />
+    </div>
+  ) : null;
+};
 
-export default GlobalLoadingSpinner
+export default GlobalLoadingSpinner;
